32cfb84c6706db424e8aebb07b9dbbc54ad8c805
Xarun <Xarun@users.noreply.github.com>
2016-05-01 12:05:05 +0200

    smaller filesize.js

--- javascripts/misc.js
+++ javascripts/misc.js
@@ -1,7 +1,12 @@
-/*
-  2016
-  @version 3.2.1
-  http://filesizejs.com/
+/**
+ * http://filesizejs.com/
+ *
+ * filesize
+ *
+ * @copyright 2016 Jason Mulligan <jason.mulligan@avoidwork.com>
+ * @license BSD-3-Clause
+ * @version 3.2.1
  */
-"use strict";!function(a){function b(a){var b=arguments.length<=1||void 0===arguments[1]?{}:arguments[1],e=[],f=0,g=void 0,h=void 0,i=void 0,j=void 0,k=void 0,l=void 0,m=void 0,n=void 0,o=void 0,p=void 0,q=void 0;if(isNaN(a))throw new Error("Invalid arguments");return i=b.bits===!0,o=b.unix===!0,h=b.base||2,n=void 0!==b.round?b.round:o?1:2,p=void 0!==b.spacer?b.spacer:o?"":" ",q=b.symbols||b.suffixes||{},m=b.output||"string",g=void 0!==b.exponent?b.exponent:-1,l=Number(a),k=0>l,j=h>2?1e3:1024,k&&(l=-l),0===l?(e[0]=0,e[1]=o?"":i?"b":"B"):((-1===g||isNaN(g))&&(g=Math.floor(Math.log(l)/Math.log(j)),0>g&&(g=0)),g>8&&(g=8),f=2===h?l/Math.pow(2,10*g):l/Math.pow(1e3,g),i&&(f=8*f,f>j&&8>g&&(f/=j,g++)),e[0]=Number(f.toFixed(g>0?n:0)),e[1]=10===h&&1===g?i?"kb":"kB":d[i?"bits":"bytes"][g],o&&(e[1]=e[1].charAt(0),c.test(e[1])&&(e[0]=Math.floor(e[0]),e[1]=""))),k&&(e[0]=-e[0]),e[1]=q[e[1]]||e[1],"array"===m?e:"exponent"===m?g:"object"===m?{value:e[0],suffix:e[1],symbol:e[1]}:e.join(p)}var c=/^(b|B)$/,d={bits:["b","Kb","Mb","Gb","Tb","Pb","Eb","Zb","Yb"],bytes:["B","KB","MB","GB","TB","PB","EB","ZB","YB"]};"undefined"!=typeof exports?module.exports=b:"function"==typeof define&&define.amd?define(function(){return b}):a.filesize=b}("undefined"!=typeof window?window:global);
-//# sourceMappingURL=filesize.min.js.map
+(function(r){function e(e){var d=1>=arguments.length||void 0===arguments[1]?{}:arguments[1],a=[],c=0,b=void 0,h=void 0,f=void 0,k=void 0,m=void 0,l=void 0,n=void 0,g=void 0,p=void 0,q=void 0;if(isNaN(e))throw Error("Invalid arguments");f=!0===d.a;g=!0===d.s;h=d.g||2;n=void 0!==d.round?d.round:g?1:2;p=void 0!==d.c?d.c:g?"":" ";q=d.o||d.suffixes||{};l=d.l||"string";b=void 0!==d.b?d.b:-1;c=Number(e);m=0>c;k=2<h?1E3:1024;m&&(c=-c);if(0===c)a[0]=0,a[1]=g?"":f?"b":"B";else{if(-1===b||isNaN(b))b=Math.floor(Math.log(c)/
+Math.log(k)),0>b&&(b=0);8<b&&(b=8);c=2===h?c/Math.pow(2,10*b):c/Math.pow(1E3,b);f&&(c*=8,c>k&&8>b&&(c/=k,b++));a[0]=Number(c.toFixed(0<b?n:0));a[1]=10===h&&1===b?f?"kb":"kB":t[f?"bits":"bytes"][b];g&&(a[1]=a[1].charAt(0),u.test(a[1])&&(a[0]=Math.floor(a[0]),a[1]=""))}m&&(a[0]=-a[0]);a[1]=q[a[1]]||a[1];return"array"===l?a:"exponent"===l?b:"object"===l?{value:a[0],m:a[1],symbol:a[1]}:a.join(p)}var u=/^(b|B)$/,t={a:"b Kb Mb Gb Tb Pb Eb Zb Yb".split(" "),h:"B KB MB GB TB PB EB ZB YB".split(" ")};"undefined"!==
+typeof exports?module.i=e:"function"===typeof define&&define.f?define(function(){return e}):r.j=e})("undefined"!==typeof window?window:global);
